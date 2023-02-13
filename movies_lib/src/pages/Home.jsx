d635c61ab95e1a9188b7e43bb435bdee10dd567b
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-13 20:27:50 -0300

    Renderizando dados da API

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard';
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -26,10 +27,15 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {topMovies && topMovies.map((movie) => (
-        movie.title
+    <div className='container'>
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+      {topMovies.length === 0 && <p>Carregando...</p>}
+      {topMovies.length > 0 && topMovies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie}/>
       ))}
+      </div>
+      
     </div>
   )
 }
