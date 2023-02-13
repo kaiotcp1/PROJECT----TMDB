ae8b7ec66a38b0520f1917f89a3ff88ab07ad8fe
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-13 20:07:19 -0300

    Fetch API

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+console.log(moviesURL);
+console.log(apiKey);
+
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();  // transformando em objeto javascript
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    
+    getTopRatedMovies(topRatedUrl);
+
+  }, []);
+
   return (
-    <div>Home</div>
+    <div>
+      {topMovies && topMovies.map((movie) => (
+        movie.title
+      ))}
+    </div>
   )
 }
 
